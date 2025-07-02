0acf3246acec8b73f0787d980c4c8aea454e71bf
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-07-02 15:56:15 -0400

    refactor(Bot): support sending via `delegateTo`

--- bot.py
+++ bot.py
@@ -24,7 +24,8 @@ else:
     ), "Signer must either be a signer in Safe, or a delegate"
 
 TOKEN = tokens[os.environ["GRANT_TOKEN_SYMBOL"]]
-CLAIM_THRESHOLD = Decimal(os.environ['GRANT_CLAIM_THRESHOLD'])
+CLAIM_THRESHOLD = Decimal(os.environ["GRANT_CLAIM_THRESHOLD"])
+RECEIVER = os.environ.get("GRANT_CLAIM_RECEIVER")
 
 
 @bot.on_startup()
@@ -44,14 +45,16 @@ def setup(_):
 
 @bot.cron(os.environ.get("GRANT_CHECK_FREQUENCY", "*/5 * * * *"))
 def available(_):
-    return Decimal(grant.balanceOf(grantee)) / 10**Decimal(TOKEN.decimals())
+    return Decimal(grant.balanceOf(grantee)) / 10 ** Decimal(TOKEN.decimals())
 
 
 if isinstance(grantee, SafeAccount):
 
     @bot.on_metric("available", ge=CLAIM_THRESHOLD)
     async def execute_claim(available: Decimal):
-        claim_amount = int((available - (available % CLAIM_THRESHOLD)) * 10**TOKEN.decimals())
+        claim_amount = int(
+            (available - (available % CLAIM_THRESHOLD)) * 10 ** TOKEN.decimals()
+        )
 
         nonce_to_replace = None
         if bot.state.claim_in_progress:  # Another task is claiming
@@ -60,7 +63,10 @@ if isinstance(grantee, SafeAccount):
                     try:
                         decoded = grant.downgrade.decode(safe_tx.data)[1]
                     except Exception:
-                        decoded = {}
+                        try:
+                            decoded = grant.downgradeTo.decode(safe_tx.data)[1]
+                        except Exception:
+                            decoded = {}
 
                     if (amount := decoded.get("amount")) == claim_amount:
                         return  # Transaction already exists
@@ -70,25 +76,44 @@ if isinstance(grantee, SafeAccount):
                         break
 
         bot.state.claim_in_progress = True
-        txn = grant.downgrade.as_transaction(
-            claim_amount,
-            sender=grantee,
-            # NOTE: Gas limit doesn't matter, but bypasses gas estimation error
-            gas_limit=200_000,
-            nonce=nonce_to_replace,  # if `None`, proposed as new SafeTx
-        )
+        if RECEIVER:
+            txn = grant.downgradeTo.as_transaction(
+                RECEIVER,
+                claim_amount,
+                sender=grantee,
+                # NOTE: Gas limit doesn't matter, but bypasses gas estimation error
+                gas_limit=200_000,
+                nonce=nonce_to_replace,  # if `None`, proposed as new SafeTx
+            )
+        else:
+            txn = grant.downgrade.as_transaction(
+                claim_amount,
+                sender=grantee,
+                # NOTE: Gas limit doesn't matter, but bypasses gas estimation error
+                gas_limit=200_000,
+                nonce=nonce_to_replace,  # if `None`, proposed as new SafeTx
+            )
         grantee.propose(txn, submitter=bot.signer)
         # NOTE: SafeTx submitted but not broadcast yet
-
 
 else:  # bot.signer == grantee
 
     @bot.on_metric("available", ge=CLAIM_THRESHOLD)
     async def execute_claim(available: Decimal):
-        claim_amount = int((available - (available % CLAIM_THRESHOLD)) * 10**TOKEN.decimals())
+        claim_amount = int(
+            (available - (available % CLAIM_THRESHOLD)) * 10 ** TOKEN.decimals()
+        )
         if not bot.state.claim_in_progress:
             bot.state.claim_in_progress = True
-            grant.downgrade(claim_amount, sender=bot.signer, confirmations_required=0)
+            if RECEIVER:
+                grant.downgradeTo(
+                    RECEIVER, claim_amount, sender=bot.signer, confirmations_required=0
+                )
+
+            else:
+                grant.downgrade(
+                    claim_amount, sender=bot.signer, confirmations_required=0
+                )
             # We have successfully claimed if transaction broadcasts
 
 
